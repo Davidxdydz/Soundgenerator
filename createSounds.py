93c4c2fa69cbc6d9f76d65d8154c105ff0c93113
Moritz Milla <moritz.milla@tum.de>
2021-03-16 23:30:38 +0100

    Added first version of soundnet.
    Co-authored-by: david.p.berger@gmx.de <david.p.berger@gmx.de>

--- createSounds.py
+++ createSounds.py
@@ -1,1 +1,53 @@
 import numpy as np
+import soundnet
+import matplotlib.pyplot as plt
+
+"""
+{
+    "python.formatting.provider": "black"
+}
+"""
+SAMPLE_FREQUENCY = 2 ** 8
+
+
+def sample(f, d=1, sf=SAMPLE_FREQUENCY):
+    """
+    Samples function f for duration d at sampling frequency sf
+
+    Args:
+        f: function that gives amplitude between -1 and 1 at time t
+        d: duration of the sample
+        sf: sampling frequency
+
+    Returns: numpy array of sampled audio
+
+    """
+    t = np.linspace(0, d, int(sf * d))
+    return np.vectorize(f)(t)
+
+
+def sample_sin(frequency, duration=1):
+    return sample(lambda t: np.sin(t * 2 * np.pi * frequency), duration)
+
+def normalize_complex(arr):
+    a = np.abs(arr)
+    ma = np.max(a)
+    arr /=ma
+    return arr
+
+if __name__ == "__main__":
+
+    freq_labels = np.arange(10, 100, 10, dtype=float)
+    # TODO samplefrequency
+    samples = np.array([normalize_complex(np.fft.rfft(sample_sin(f)))[:-1] for f in freq_labels]).reshape(
+        (-1, SAMPLE_FREQUENCY//2, 1)
+    )
+    freq_labels /= 100
+    plt.plot(abs(samples[8, :, 0]), label = "original")
+    autoencoder, encoder, decoder = soundnet.build_model((SAMPLE_FREQUENCY//2, 1), 20)
+    print(autoencoder.summary())
+    soundnet.train_autoencoder(autoencoder, samples, freq_labels)
+    predictions = autoencoder.predict(samples)
+    plt.plot(abs(predictions[0][0,..., 0]), label="predicted")
+    plt.legend()
+    plt.savefig("sin.png")
---
+++ soundnet.py
@@ -0,0 +1,73 @@
+from tensorflow.keras.layers import (
+    Dense,
+    Input,
+    Conv1D,
+    Flatten,
+    MaxPool1D,
+    UpSampling1D,
+)
+from tensorflow.keras.models import Model
+
+
+def build_encoder(input_shape, embedded_size):
+
+    # (n,)
+    encoder_input = Input(input_shape)
+    # (n, 16)
+    encoder_hidden = Conv1D(32, 8, padding="same", activation="tanh")(encoder_input)
+    # (n / 4, 16)
+    encoder_hidden = MaxPool1D(4, padding="same")(encoder_hidden)
+    # (n / 4, 32)
+    encoder_hidden = Conv1D(64, 8, padding="same", activation="tanh")(encoder_hidden)
+    # (n / 16, 32)
+    encoder_hidden = MaxPool1D(4, padding="same")(encoder_hidden)
+
+    # (n / 16, embedded_size)
+    encoder_out = Dense(embedded_size, activation="tanh")(encoder_hidden)
+
+    return Model(encoder_input, encoder_out)
+
+
+def build_decoder(feature_shape):
+
+    decoder_input = Input(feature_shape)
+    decoder_hidden = Conv1D(32, 8, padding="same", activation="tanh")(decoder_input)
+    decoder_hidden = UpSampling1D(4)(decoder_input)
+    decoder_hidden = Conv1D(16, 8, padding="same", activation="tanh")(decoder_hidden)
+    decoder_hidden = UpSampling1D(4)(decoder_hidden)
+    decoder_out = Conv1D(1, 8, padding="same", activation="tanh")(decoder_hidden)
+    return Model(decoder_input, decoder_out)
+
+
+def build_freq_classifier(feature_shape):
+    freq_input = Input(feature_shape)
+    freq_flatten = Flatten()(freq_input)
+    freq_classifier = Dense(8, activation="tanh")(freq_flatten)
+    freq_classifier = Dense(1, activation="tanh")(freq_classifier)
+    return Model(freq_input, freq_classifier)
+
+
+def build_model(input_shape, embedded_size):
+
+    encoder = build_encoder(input_shape, embedded_size)
+    decoder = build_decoder(encoder.output_shape[1:])
+    freq_classifier = build_freq_classifier(encoder.output_shape[1:])
+
+    autoencoder_input = Input(input_shape)
+    autoencoder_enc = encoder(autoencoder_input)
+    autoencoder_freq = freq_classifier(autoencoder_enc)
+    autoencoder_dec = decoder(autoencoder_enc)
+
+    return (
+        Model(autoencoder_input, [autoencoder_dec, autoencoder_freq]),
+        encoder,
+        decoder,
+    )
+
+
+def train_autoencoder(autoencoder, input_samples, freq_labels):
+
+    autoencoder.compile(loss="mse", optimizer="adam")
+
+
+    autoencoder.fit(input_samples, [input_samples, freq_labels], epochs = 100)
