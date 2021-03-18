0ff457999ab77fbbc175c2455959a376171d80c4
= <=>
2021-03-18 15:23:36 +0100

    moved plotting to own module

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sound_generator.visualize import plot_waveforms
 import numpy as np
 from sound_generator import soundnet, data_processing, global_configuration
 from function_generator import SineGenerator, UniformSampler
@@ -14,13 +15,6 @@ if __name__ == "__main__":
         samples
     )
 
-    # plotting for reference
-    # TODO move this
-    fig, axes = plt.subplots(3, 3, sharex=True, sharey=True, figsize=(15, 15))
-    for n, sample in enumerate(samples):
-        ax = axes[n % 3][n // 3]
-        ax.plot(sample, color="orange")
-
     # now build and train the model
     autoencoder, encoder, decoder = soundnet.build_model(magnitudes.shape[1:], 64)
     print(autoencoder.summary())
@@ -30,16 +24,12 @@ if __name__ == "__main__":
     # TODO split into training and validation
     predictions = autoencoder.predict(magnitudes)
     recovered = []
+    frequency_prediction = []
     for n, (magnitudes, f, phases, params) in enumerate(
         zip(predictions[0], predictions[1], phases, params)
     ):
         # TODO maybe move this postprocessing into a function
-        recovered.append(data_processing.postprocess(magnitudes[:-padding], phases, params))
-        ax = axes[n % 3][n // 3]
-        ax.plot(recovered[-1], color="blue")
-        ax.set_title(f"Predicted: {int(f*90+10)}Hz")
-    plt.legend(["original", "predicted"])
-    plt.tight_layout()
-    plt.savefig("sinetest.png")
-    plt.show()
-    # TODO why is reconstructed always smaller in amplitude? normalization or architecture
+        recovered.append(data_processing.postprocess(magnitudes, phases, params, padding))
+        frequency_prediction.append(f*90+10)
+    
+    plot_waveforms(samples,recovered,frequencies,frequency_prediction)
---
+++ sound_generator/visualize.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sound_generator.global_configuration import SAMPLE_FREQUENCY
+
+def plot_waveforms(original_sounds, predicted_sounds, original_frequencies, predicted_frequencies, t_range = (0,1)):
+    """
+    plots orignal sounds against predicted sounds in a new window
+
+    Args:
+        original_sounds: List of original sounds
+        predicted_souds: List of predicted sounds
+        original_frequencies: List of original frequencies
+        predicted_frequencies: List of predicted frequencies
+    
+    Returns:
+        Nothing
+    """
+    size = len(original_sounds)
+    width = int(np.sqrt(size))
+    height = size//width+1
+    fig, axes = plt.subplots(height, width, sharex=True, sharey=True, figsize=(15, 15))
+    t = np.linspace(*t_range,SAMPLE_FREQUENCY)
+    for n, (os,ps,of,pf) in enumerate(zip(original_sounds,predicted_sounds,original_frequencies,predicted_frequencies)):
+        x = n%width
+        y = n//width
+        ax = axes[y][x]
+        ax.plot(t, os, color="orange")
+        ax.plot(t, ps, color="blue")
+        ax.set_title(f"{int(of)}Hz, predicted: {int(pf)}Hz")
+    plt.legend()
+    plt.show()
