7eb5d49155ae8ebb106fcc636b35e56e7c6e2d6d
David Berger <david.p.berger@gmx.de>
2021-03-20 14:32:58 +0100

    complete overhaul

--- sound_generator/soundnet.py
+++ sound_generator/soundnet.py
@@ -1,62 +1,192 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
 from tensorflow.keras.layers import (
     Dense,
     Input,
     Conv1D,
     Flatten,
     UpSampling1D,
+    Add,
+    Lambda
 )
 from tensorflow.keras.models import Model
 from sound_generator.global_configuration import TRAINING_EPOCHS, FEATURE_FIT_ITERATIONS,EMBEDDED_SIZE
 import numpy as np
 
 
-def build_encoder(input_shape):
+"""
+all flat
+
+        samples n   fft-mag n/2     fft-pha n/2 1
+layer1:     | a         | b             | b     1
+layer2:     | m         | m             | m     1
+            -----------------------------
+                        |
+                       add m                    2
+layer3:                 | c                     2
+embedded:               | d                     2
+                        ---------------------------------
+-layer3:                | c                     3       |
+                        | m                     3       |
+            -----------------------------               |
+-layer2:    | a         | b             | b     4       |
+-layer1:    | n         | n/2           | n/2   4     classifier
+            |           -----------------       4       |
+            |                   |               4       |
+            |                  ifft             4       |
+            |                   |               4       |
+            ---------------------               4       |
+                    add                         4       |
+                mse against samples                 mse against freq
+
+"""
+a = 128
+b = 64
+m = 64
+c = 64
+d = 32
+
+
+
+def build_sample_encoder(input_shape):
     """
-    Builds an encoder model for use in an autoencoder.
-
-    Args:
-        input_shape: Tuple with network input shape
-
-    Returns:
-        Encoder model with specified dimensions.
+    TODO
     """
-
-    # TODO increase params, atm only 26k params
     encoder_input = Input(input_shape)
-    encoder_hidden = Conv1D(64, 4, padding="same", activation="relu")(encoder_input)
-    encoder_hidden = Dense(32, activation="relu")(encoder_hidden)
-    encoder_hidden = Conv1D(16, 4, padding="same", activation="relu")(encoder_hidden)
+    encoder_hidden = Dense(a, activation = "relu")(encoder_input)
+    encoder_hidden = Dense(m,activation = "relu")(encoder_hidden)
+    return Model(encoder_input,encoder_hidden,name = "SampleEncoder")
 
-    # TODO make this constant maybe so that input shape is not correlated with embedded_size
-    # (n / 16, embedded_size)
-    encoder_out = Dense(EMBEDDED_SIZE, activation="relu")(encoder_hidden)
-
-    return Model(encoder_input, encoder_out, name="Encoder")
-
-
-def build_decoder(feature_shape):
+def build_magnitude_encoder(input_shape):
     """
-    Builds an decoder model for use in an autoencoder.
+    TODO
+    """
+    encoder_input = Input(input_shape)
+    encoder_hidden = Dense(b, activation = "relu")(encoder_input)
+    encoder_hidden = Dense(m,activation = "relu")(encoder_hidden)
+    return Model(encoder_input,encoder_hidden,name = "MagnitudeEncoder")
 
-    Args:
-        feature_shape: Tuple containing the shape of the embedded input space.
+def build_phase_encoder(input_shape):
+    """
+    TODO
+    """
+    encoder_input = Input(input_shape)
+    encoder_hidden = Dense(b, activation = "relu")(encoder_input)
+    encoder_hidden = Dense(m,activation = "relu")(encoder_hidden)
+    return Model(encoder_input,encoder_hidden,name = "PhaseEncoder")
 
-    Returns:
-        Decoder model with specified dimensions.
+def build_inner_encoder():
+    """
+    TODO
     """
 
-    decoder_input = Input(feature_shape)
-    decoder_hidden = Conv1D(16, 4, padding="same", activation="relu")(decoder_input)
-    decoder_hidden = Dense(32, activation="relu")(decoder_input)
-    decoder_hidden = Conv1D(64, 4, padding="same", activation="relu")(decoder_hidden)
-    decoder_out = Conv1D(1, 4, padding="same", activation="relu")(decoder_hidden)
+    sample_encoder_output = Input((m,))
+    magnitude_encoder_output = Input((m,))
+    phase_encoder_output = Input((m,))
+    added = Add()([sample_encoder_output,magnitude_encoder_output,phase_encoder_output])
 
-    return Model(decoder_input, decoder_out, name="Decoder")
+    encoder_hidden = Dense(c, activation="relu")(added)
+    encoder_out = Dense(d, activation="relu")(encoder_hidden)
+
+    return Model(inputs = [sample_encoder_output,magnitude_encoder_output,phase_encoder_output], outputs = encoder_out, name="InnerEncoder")
 
 
-def build_freq_classifier(feature_shape):
+def build_inner_decoder():
+    """
+    TODO
+    """
+
+    decoder_input = Input((d,))
+    decoder_hidden = Dense(c,activation="relu")(decoder_input)
+    decoder_out = Dense(m,activation="relu")(decoder_hidden)
+
+    return Model(decoder_input, decoder_out, name="InnerDecoder")
+
+def build_sample_decoder(sample_shape):
+    """
+    TODO
+    """
+
+    decoder_input = Input((m,))
+    decoder_hidden = Dense(a,activation="relu")(decoder_input)
+    decoder_output = Dense(*sample_shape,activation="relu")(decoder_hidden)
+    return Model(decoder_input,decoder_output,name = "SampleDecoder")
+
+def build_magnitude_decoder(magnitude_shape):
+    """
+    TODO
+    """
+
+    decoder_input = Input((m,))
+    decoder_hidden = Dense(b,activation="relu")(decoder_input)
+    decoder_output = Dense(*magnitude_shape,activation="relu")(decoder_hidden)
+    return Model(decoder_input,decoder_output,name = "MagnitudeDecoder")
+
+def build_phase_decoder(phase_shape):
+    """
+    TODO
+    """
+
+    decoder_input = Input((m,))
+    decoder_hidden = Dense(b,activation="relu")(decoder_input)
+    decoder_output = Dense(*phase_shape,activation="relu")(decoder_hidden)
+    return Model(decoder_input,decoder_output,name = "PhaseDecoder")
+
+def polar_to_complex(inp):
+    r = inp[0]
+    p = inp[1]
+    return tf.cast(r,tf.complex64)* tf.complex(tf.math.cos(p),tf.math.sin(p))
+
+
+def build_outer_decoder(sample_shape,magnitude_shape,phase_shape):
+    """
+    TODO
+    """
+
+    inner_decoder_output = Input((m,))
+
+    sample_decoder = build_sample_decoder(sample_shape)(inner_decoder_output)
+    magnitude_decoder = build_magnitude_decoder(magnitude_shape)(inner_decoder_output)
+    phase_decoder = build_phase_decoder(phase_shape)(inner_decoder_output)
+
+
+    fft_layer = Lambda(polar_to_complex,dtype = 'complex64')([magnitude_decoder,phase_decoder])
+    time_layer = Lambda(tf.signal.irfft)(fft_layer)
+    adding_layer = Add()([sample_decoder,time_layer])
+
+    return Model(inner_decoder_output,adding_layer,name = "OuterDecoder")
+
+def build_encoder(sample_shape,magnitude_shape,phase_shape):
+    """
+    TODO
+    """
+    sample_input = Input(sample_shape)
+    magnitude_input = Input(magnitude_shape)
+    phase_input = Input(phase_shape)
+
+    sample_encoder = build_sample_encoder(sample_shape)(sample_input)
+    magnitude_encoder = build_magnitude_encoder(magnitude_shape)(magnitude_input)
+    phase_encoder = build_phase_encoder(phase_shape)(phase_input)
+
+    inner_encoder = build_inner_encoder()
+    connected = inner_encoder([sample_encoder,magnitude_encoder,phase_encoder])
+    return Model(inputs = [sample_input,magnitude_input,phase_input],outputs = connected, name = "Encoder")
+
+def build_decoder(sample_shape,magnitude_shape,phase_shape):
+    """
+    TODO
+    """
+    feature_space = Input((d,))
+
+    inner_decoder = build_inner_decoder()(feature_space)
+
+    outer_decoder = build_outer_decoder(sample_shape,magnitude_shape,phase_shape)(inner_decoder)
+
+    return Model(feature_space,outer_decoder,name = "Decoder")
+
+
+def build_freq_classifier():
     """
     Builds a classifier model to investigate frequencies in embedded space.
 
@@ -68,44 +198,22 @@ def build_freq_classifier(feature_shape):
         point represents.
     """
 
-    freq_input = Input(feature_shape)
+    freq_input = Input((d,))
     freq_flatten = Flatten()(freq_input)
     freq_classifier = Dense(16, activation="relu")(freq_flatten)
     freq_classifier = Dense(1, activation="relu")(freq_classifier)
     return Model(freq_input, freq_classifier, name="FreqClassifier")
 
+def build_autoencoder(sample_shape,magnitude_shape,phase_shape):
+    sample_input = Input(sample_shape)
+    magnitude_input = Input(magnitude_shape)
+    phase_input = Input(phase_shape)
 
-def build_model(input_shape):
-    """
-    Builds a frequency autoencoder model with a side output for
-    that estimates the frequncy of the input sample.
+    encoder = build_encoder(sample_shape,magnitude_shape,phase_shape)([sample_input,magnitude_input,phase_input])
+    decoder = build_decoder(sample_shape,magnitude_shape,phase_shape)(encoder)
+    freq_classifier = build_freq_classifier()(encoder)
 
-    Args:
-        input_shape: Tuple with network input and output shape
-
-    Returns:
-        A Quadruple containing the complete autoencoder, the encoder and
-        decoder and the freuqency classifier.
-    """
-
-    encoder = build_encoder(input_shape)
-    decoder = build_decoder(encoder.output_shape[1:])
-    freq_classifier = build_freq_classifier(encoder.output_shape[1:])
-
-    autoencoder_input = Input(input_shape)
-    autoencoder_enc = encoder(autoencoder_input)
-    autoencoder_freq = freq_classifier(autoencoder_enc)
-    autoencoder_dec = decoder(autoencoder_enc)
-
-    return (
-        Model(
-            autoencoder_input, [autoencoder_dec, autoencoder_freq], name="autoencoder"
-        ),
-        encoder,
-        decoder,
-        freq_classifier,
-    )
-
+    return Model(inputs = [sample_input,magnitude_input,phase_input],outputs = [decoder,freq_classifier],name = "Autoencoder")
 
 def freeze_model(model, freeze):
     """
@@ -168,7 +276,7 @@ def match_features_space_frequency(encoder, frequency_classifier, sample, freque
     return transform_model.predict(feature_space_sample)
 
 
-def train_autoencoder(autoencoder, input_samples, freq_labels):
+def train_autoencoder(autoencoder, samples,magnitudes,phases, freq_labels):
     """
     Trains a model produced by the build_model function.
 
@@ -179,4 +287,4 @@ def train_autoencoder(autoencoder, input_samples, freq_labels):
     """
 
     autoencoder.compile(loss="mse", optimizer="adam")
-    autoencoder.fit(input_samples, [input_samples, freq_labels], epochs=TRAINING_EPOCHS)
+    autoencoder.fit([samples,magnitudes,phases], [samples, freq_labels], epochs=TRAINING_EPOCHS)
